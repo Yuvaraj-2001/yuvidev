7b8d0d8c857df9777e9f2ea6d46435b214f9af17
Yuvaraj-2001 <yuvayuvaraj720444@gmail.com>
2023-08-28 23:26:28 +0530

    first

---
+++ script/main.js
@@ -0,0 +1,68 @@
+function header_adj() {
+	if ($(window).width() < 767) {
+		var header_height = $(".header").outerHeight();
+		$(".nav-wrap .nav-list").css({ "padding-top": header_height + "px" });
+	} else {
+		$(".nav-wrap .nav-list").css({ "padding-top": "0" });
+	}
+}
+function submenu_toggle() {
+	if ($(window).width() < 767) {
+		$(".nav-list li.with-submenu")
+			.off()
+			.click(function () {
+				$(this).toggleClass("is-open");
+				$(".submenu").slideToggle("slow");
+			});
+	}
+}
+$(document).ready(function () {
+	header_adj();
+	submenu_toggle();
+	if ($(window).width() < 767) {
+		$(".hamburger")
+			.off()
+			.click(function () {
+				$(this).toggleClass("is-active");
+				$("body,html").toggleClass("sidebar-open");
+				$(".nav-wrap").toggleClass("is-open");
+			});
+
+		$(".overlay")
+			.off()
+			.click(function () {
+				$(".hamburger").removeClass("is-active");
+				$("body,html").removeClass("sidebar-open");
+				$(".nav-wrap").removeClass("is-open");
+			});
+	} else {
+		$(".hamburger").removeClass("is-active");
+		$("body,html").removeClass("sidebar-open");
+		$(".nav-wrap").removeClass("is-open");
+	}
+});
+$(window).on("resize", function () {
+	header_adj();
+	submenu_toggle();
+	if ($(window).width() < 767) {
+		$(".hamburger")
+			.off()
+			.click(function () {
+				$(this).toggleClass("is-active");
+				$("body,html").toggleClass("sidebar-open");
+				$(".nav-wrap").toggleClass("is-open");
+			});
+
+		$(".overlay")
+			.off()
+			.click(function () {
+				$(".hamburger").removeClass("is-active");
+				$("body,html").removeClass("sidebar-open");
+				$(".nav-wrap").removeClass("is-open");
+			});
+	} else {
+		$(".hamburger").removeClass("is-active");
+		$("body,html").removeClass("sidebar-open");
+		$(".nav-wrap").removeClass("is-open");
+	}
+});
